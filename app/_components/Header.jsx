5b3ce4da893b3c7dbcb5805c6b9830def01f118b
Krishna <coderks36@gmail.com>
2025-04-02 19:20:13 +0530

    Authentication implemented

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,18 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button';
+import { useUser,UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+
+
+
 
 function Header() {
+     
+  const {user,isSignedIn}=useUser();
+
   return (
     <div className='p-5 flex justify-between items-center border shadow-md rounded-sm'>
         <Image src={'./logo.svg'}
@@ -10,7 +20,9 @@ function Header() {
         width={100}
         height={100}
         />
-        <Button>Get Started</Button>
+        {isSignedIn?
+        <UserButton/>:<Link href={'/sign-in'}><Button className=" border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700">Get Started</Button></Link>}
+        
     </div>
   )
 }
