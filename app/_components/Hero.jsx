5c8ba6e50367c58bb2931b1308226af77b063b44
Krishna <coderks36@gmail.com>
2025-04-02 22:13:01 +0530

    Authenticationi done on hero

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -11,7 +11,7 @@ export default function
   <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
     <div className="mx-auto max-w-prose text-center">
       <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
-        Mange Your Expense    
+        Manage Your Expense    
         <strong className="text-indigo-600 text-4xl sm:text-2xl"> Control Your Money</strong>
        
       </h1>
