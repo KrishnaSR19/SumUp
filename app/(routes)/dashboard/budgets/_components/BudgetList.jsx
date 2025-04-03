75b4cd195c5e82f754ccf6ac2e4abe2465f9ccc7
Krishna <coderks36@gmail.com>
2025-04-03 23:35:02 +0530

    Budget Component Setup

---
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CreateBudget from './CreateBudget'
+
+function BudgetList() {
+  return (
+    <div className='mt-7'>
+        <div>
+        <CreateBudget/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default BudgetList
---
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CreateBudget() {
+  return (
+    <div >
+        <div className='bg-slate-100 p-10 rounded-md items-center flex flex-col border border-dashed cursor-pointer'>
+            <h2 className='text-3xl'>+</h2>
+            <h2>Create New Budget</h2>
+        </div>
+    </div>
+  )
+}
+
+export default CreateBudget
