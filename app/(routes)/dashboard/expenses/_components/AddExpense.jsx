d8ae19df98b8754ef8debdebd943443fb4dd82e7
Krishna <coderks36@gmail.com>
2025-04-06 00:39:56 +0530

    toast and refresh data function added in AddExpense

--- app/(routes)/dashboard/expenses/_components/AddExpense.jsx
+++ app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -1,18 +1,48 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { db } from "@/utils/dbConfig";
+import { Budgets, Expenses } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment/moment";
+import { useParams } from "next/navigation";
 import React, { useState } from "react";
+import { toast } from "sonner";
 
-function AddExpense() {
+function AddExpense({budgetId,user,refreshData}) {
+  const [name, setName] = useState();
+  const [amount, setAmount] = useState();
+  // const { user } = useUser();
+  const params = useParams(); //  use the hook
 
-    const [name,setName]=useState();
-    const [amount,setAmount]=useState();
+  const addNewExpense = async () => {
+    try {
+      const result = await db
+        .insert(Expenses)
+        .values({
+          name:name,
+          amount: parseFloat(amount),
+          budgetId: budgetId,
+          createdAt:moment().format('DD/MM/YYYY')
+            
+        })
+        .returning({insertedId:Budgets.id});
 
-
+      console.log("Data Saved Bhai",result);
+      if (result) {
+        refreshData();
+        //show success message
+        toast.success("New Expense Added");
+      }
+      // Show error message
+    } catch (error) {
+      toast.error("Error creating budget: " + error.message); // Show error message if insertion fails
+    }
+  };
 
   return (
     <div className="border p-3 ml-3 rounded-lg ">
-        <h2 className="font-bold text-lg">Add Expense</h2>
-     
+      <h2 className="font-bold text-lg">Add Expense</h2>
+
       <div className="mt-2">
         <h2 className="text-black font-medium my-1">Expense Name</h2>
         <Input
@@ -28,7 +58,13 @@ function AddExpense() {
           onChange={(e) => setAmount(e.target.value)}
         />
       </div>
-      <Button disabled={!(name&&amount)} className="mt-3 w-full bg-indigo-600">Add</Button>
+      <Button
+        disabled={!(name && amount)}
+        onClick={addNewExpense}
+        className="mt-3 w-full bg-indigo-600"
+      >
+        Add Expense
+      </Button>
     </div>
   );
 }
