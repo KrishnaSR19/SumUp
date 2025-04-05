667643133326176d54eb712649cc9aa2a8c6ed2c
Krishna <coderks36@gmail.com>
2025-04-05 19:44:48 +0530

    get budger list added

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -1,24 +1,28 @@
 "use client";
-import { db } from '@/utils/dbConfig';
-import { Budgets, Expenses } from '@/utils/schema';
-import { useUser } from '@clerk/nextjs';
-import { and, eq, getTableColumns, sql } from 'drizzle-orm';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation'; // ✅ import useParams
-import BudgetItem from '../../budgets/_components/BudgetItem';
-import AddExpense from '../_components/AddExpense';
+import { db } from "@/utils/dbConfig";
+import { Budgets, Expenses } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { and, desc, eq, getTableColumns, sql } from "drizzle-orm";
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation"; // ✅ import useParams
+import BudgetItem from "../../budgets/_components/BudgetItem";
+import AddExpense from "../_components/AddExpense";
+import ExpenseListTable from "../_components/ExpenseListTable";
 
 function ExpensesScreen() {
   const { user } = useUser();
   const params = useParams(); // ✅ use the hook
   const [budgetInfo, setBudgetInfo] = useState(null);
+  const [expensesList,setExpensesList]=useState([])
 
   useEffect(() => {
     if (user && params?.id) {
       getBudgetInfo();
+
     }
   }, [user, params]);
 
+  //Get Budget Information
   const getBudgetInfo = async () => {
     const userEmail = user?.primaryEmailAddress?.emailAddress;
     const currentBudgetId = Number(params.id); // ✅ renamed here
@@ -44,10 +48,23 @@ function ExpensesScreen() {
       )
       .groupBy(Budgets.id);
 
-    console.log("🎯 Final result from DB:", result);
+    // console.log(" Final result from DB:", result);
     setBudgetInfo(result?.[0] || null);
+    getExpenseList();
+   
   };
 
+
+  //Get Budget List
+  const getExpenseList=async()=>{
+    const result = await db.select().from(Expenses)
+    .where(eq(Expenses.budgetId,params.id))
+    .orderBy(desc(Expenses.id));
+   
+    setExpensesList(result);
+    console.log(result);
+
+  }
   return (
     <div className="p-10">
       <h2 className="text-2xl font-bold">My Expenses</h2>
@@ -57,11 +74,12 @@ function ExpensesScreen() {
         ) : (
           <div className="h-[150] w-full bg-slate-200 rounded-lg animate-pulse"></div>
         )}
-      <AddExpense/>
+        <AddExpense budgetId={params.id} user={user} refreshData={()=>getBudgetInfo()} />
       </div>
+      <h2 className="font-bold text-lg">Latest Expenses</h2>
+      <ExpenseListTable expensesList={expensesList}/>
     </div>
   );
-  
 }
 
 export default ExpensesScreen;
