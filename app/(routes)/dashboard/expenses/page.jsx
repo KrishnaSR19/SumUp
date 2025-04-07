ee82779d90c7123857e760e94834c2ce62bce44b
Krishna <coderks36@gmail.com>
2025-04-07 23:36:50 +0530

    expenses route

---
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpenseListTable from './_components/ExpenseListTable'
+
+function ExpenseRoute() {
+  return (
+    <div>
+        <ExpenseListTable/>
+    </div>
+  )
+}
+
+export default ExpenseRoute
