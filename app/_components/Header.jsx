04175e338a6d0519056a69399e37d7997de6967f
Krishna <coderks36@gmail.com>
2025-04-07 23:40:22 +0530

    Landing Page Header updated with new logo

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -15,11 +15,7 @@ function Header() {
 
   return (
     <div className='p-5 flex justify-between items-center border shadow-md rounded-sm'>
-        <Image src={'./logo.svg'}
-        alt="Logo"
-        width={100}
-        height={100}
-        />
+   <Link href={'/'}><Image src="/logo.png" alt="logo" width={100} height={50} priority style={{ height: "auto" }}  /></Link>
         {isSignedIn?
         <UserButton/>:<Link href={'/sign-in'}><Button className=" border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700">Get Started</Button></Link>}
         
