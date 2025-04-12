1a5f5a7fbe968c60d546a2ecd4fe56feed33028c
Krishna <coderks36@gmail.com>
2025-04-12 23:40:54 +0530

    minor changes

--- app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { BarChart, Bar, Tooltip, XAxis, YAxis, Legend } from "recharts";
+import { BarChart, Bar, Tooltip, XAxis, YAxis, Legend, ResponsiveContainer } from "recharts";
 
 function BarChartDashboard({ budgetList }) {
   return (
     <div className="border rounded-lg p-5 mr-2">
       <h2 className="font-bold mb-2">Summary</h2>
+      <ResponsiveContainer width={"80%"} height={300}>
       <BarChart
-        width={500}
-        height={300}
         data={budgetList}
         margin={{
           top: 7,
-
+         
         }}
       >
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="totalSpend" stackId="a" fill="#4845d2" />
-        <Bar dataKey="amount" stackId="a" fill="#C3C2FF" />
+        <Bar dataKey="totalSpend" fill="#7e22ce" />  // purple-600
+        <Bar dataKey="amount" fill="#c084fc" />      // purple-300
       </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
