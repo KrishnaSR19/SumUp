6b7469e77b3386899cf76d0b38695277e54d81b4
Krishna <coderks36@gmail.com>
2025-04-15 18:51:17 +0530

    budget expense page.jsx updated with dark mode classes and formated

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import BudgetList from './_components/BudgetList';
 
 function Budgets() {
   return (
-    <div>Budgets</div>
-  )
+    <div className="p-10 bg-white dark:bg-slate-900">
+      <h2 className="font-bold text-3xl text-black dark:text-white">My Budget</h2>
+      <BudgetList />
+    </div>
+  );
 }
 
-export default Budgets
+export default Budgets;
