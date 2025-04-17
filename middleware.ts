4fc4f01bd86a0519f13716d3b32b081ba4a210ff
Krishna <coderks36@gmail.com>
2025-04-17 14:39:44 +0530

    clerk middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,10 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+// Middleware configuration
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next/static|_next/image|favicon.ico|$).*)", // Ignore Next.js internals + Home `/`
+    "/(api|trpc)(.*)", // Always apply to API routes
   ],
 };
